7e22395c515fc3ae1e4c2afe66440327ece48905
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2025-09-11 09:41:42 +0500

    feat(network): Refactor NodeRole enum and integrate into Network struct

--- server/src/blockchain/network.rs
+++ server/src/blockchain/network.rs
@@ -3,6 +3,14 @@ use std::collections::HashSet;
 
 pub type Address = String;
 
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Hash)]
+pub enum NodeRole {
+    Authority,
+    Validator,
+    Reader,
+}
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Node {
     pub address: Address,
@@ -23,9 +31,49 @@ impl Node {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Hash)]
-pub enum NodeRole {
-    Authority,
-    Validator,
-    Reader,
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Network {
+    pub authority: Node,
+    pub validators: Vec<Node>,
+    pub readers: Vec<Node>,
+    pub allowed_nodes: HashSet<String>,
+}
+
+impl Network {
+    pub fn new(authority: String) -> Self {
+        let authority_node = Node::new(authority, NodeRole::Authority);
+        Self {
+            authority: authority_node,
+            validators: Vec::new(),
+            readers: Vec::new(),
+            allowed_nodes: HashSet::new(),
+        }
+    }
+
+    pub fn add_node(&mut self, address: String, role: NodeRole) {
+        let node = Node::new(address, role);
+        match node.role {
+            NodeRole::Authority => {
+                // Only one authority allowed
+                println!("Authority node already exists.");
+            }
+            NodeRole::Validator => {
+                self.allowed_nodes.insert(node.address.clone());
+                self.validators.push(node);
+            }
+            NodeRole::Reader => {
+                self.allowed_nodes.insert(node.address.clone());
+                self.readers.push(node);
+            }
+        }
+    }
+
+    pub fn change_authority(&mut self, new_authority: String) {
+        let authority_node = Node::new(new_authority, NodeRole::Authority);
+        self.authority = authority_node;
+    }
+
+    pub fn is_node_allowed(&self, address: &String) -> bool {
+        self.allowed_nodes.contains(address)
+    }
 }
