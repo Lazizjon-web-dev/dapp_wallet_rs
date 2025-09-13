958e4b868a1c16ebcb01d92db3e12584ae0f7c2e
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2025-09-13 06:13:16 +0500

    feat(block): Update Block struct to include MergeVu8 and Result imports

--- server/src/blockchain/block.rs
+++ server/src/blockchain/block.rs
@@ -1,4 +1,7 @@
-use crate::transaction::Transaction;
+use crate::{
+    blockchain::transaction::{MergeVu8, Transaction},
+    utils::error::Result,
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -9,4 +12,18 @@ pub struct Block {
     pub transactions: Vec<Transaction>,
     pub authority_signature: String,
     pub validator_signatures: Vec<String>,
+}
+
+impl Block {
+    pub fn new(transactions: Vec<Transaction>, previous_hash: String) -> Self {
+        let timestamp = Utc::now().timestamp_millis() as u128;
+        Self {
+            hash: String::new(),
+            previous_hash,
+            timestamp,
+            transactions,
+            authority_signature: String::new(),
+            validator_signatures: Vec::new(),
+        }
+    };
 }
